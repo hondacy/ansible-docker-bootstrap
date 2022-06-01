eaed8b427ee57b1bf524f7fa1e60542e6b52d5f5
Sec.Biz <18512642+hondacy@users.noreply.github.com>
2022-06-01 18:14:13 +0300

    Working compose lab

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
-import json
 import os
 import sys
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return json.dumps({"message": "Shalom there, Using Port: " + os.environ["FLASK_PORT"] })
-
+    return jsonify({"message": "Shalom, " + request.remote_addr + ", You Are Using Port: " + os.environ["FLASK_PORT"] })
 
 if __name__ == '__main__':
     flask_port = None
@@ -18,25 +18,3 @@ if __name__ == '__main__':
         print("Expected FLASK_PORT to be set. Exiting.")
         sys.exit(1)
     app.run(debug=True, host='0.0.0.0', port=flask_port)
-
-
-
-
-# @app.get("/value")
-# def get_value():
-#     return json.dumps({"value": str(r.get('value'))})
-# @app.post("/value")
-# def set_value():
-#     r.set("value", str(request.json['value']))
-
-
-# if __name__ == '__main__':
-#     redis_url = None
-#     try:
-#         redis_url = os.environ["REDIS_URL"]
-#     except KeyError:
-#         print("Expected REDIS_URL to be set. Exiting.")
-#         sys.exit(1)
-
-#     r = redis.StrictRedis(host=redis_url, port=6379, db=0)
-#     app.run(host="0.0.0.0")
